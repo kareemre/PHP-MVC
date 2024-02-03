7c310dff30a7d69dcf45b5b976bd01645b9808a5
kareem Re <lekareem21@outlook.com>
2024-02-03 13:23:22 +0200

    handling request

--- public/index.php
+++ public/index.php
@@ -2,10 +2,33 @@
 
 use Matariya\Bootstrap\Application;
 use Matariya\File\File;
+use Matariya\Http\Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Application();
+// $path = $_SERVER['REQUEST_URI'];
+// $dimp = str_contains($path, 'public') ? explode('public', $path)[1] : null;
+// echo($dimp);
+// echo Request::server('REQUEST_URI');
+// $script = dirname($_SERVER['SCRIPT_NAME']);
+// echo($script);
+// echo '<pre>';
+// $path = $_SERVER['REQUEST_urI'];
+// print_r($_SERVER);
+
+
+// echo '</pre>';
+// $requestUri = Request::server('REQUEST_URI');
+
+// echo $requestUri;
+
+// if (strpos($requestUri, '?') !== false) {
+//     list($requestUri, $queryString) = explode('?', $requestUri);
+//  }
+
+//  $url = explode('public', $requestUri)[1];
+//  var_dump($url);
 
 
 
---
+++ src/Http/Request.php
@@ -0,0 +1,82 @@
+<?php 
+namespace Matariya\Http;
+
+class Request
+{    
+    /**
+     * uri
+     *
+     * @var string
+     */
+    private $url;
+
+    
+    /**
+     * setting the url 
+     *
+     * @return void
+     */
+    public function prepareUrl()
+    {
+        $requestUri = static::server('REQUEST_URI');
+
+        if (str_contains($requestUri, '?')) {
+            list($requestUri, $queryString) = explode('?', $requestUri);
+         }
+
+         $this->url = explode('public', $requestUri)[1];
+    }
+
+    /**
+     * get the value of cleansed url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * get the value of server global value with given key
+     *
+     * @param  string $key
+     * @return mixed
+     */
+    public static function server(string $key)
+    {
+        return $_SERVER[$key];
+    }
+
+        /**
+     * Get value from get request
+     * 
+     * @param string $key
+     * @return string $value 
+     */
+    public static function get(string $key) 
+    {
+         return $_GET[$key];
+    }
+
+    /**
+     * Get value from post request
+     * 
+     * @param string $key
+     * @return string $value 
+     */
+    public static function post(string $key) 
+    {
+         return $_POST[$key];
+    }
+    
+    /**
+     * determining the request method
+     *
+     * @return string
+     */
+    public static function method()
+    {
+         return static::server('REQUEST_METHOD');
+    }
+}
