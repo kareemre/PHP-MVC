e7bdde2402b35ac493b9f7ec01b26819e4d65956
kareem Re <lekareem21@outlook.com>
2024-02-04 16:40:18 +0200

    handling routing system

--- public/index.php
+++ public/index.php
@@ -3,32 +3,15 @@
 use Matariya\Bootstrap\Application;
 use Matariya\File\File;
 use Matariya\Http\Request;
+use Matariya\Router\Route;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Application();
-// $path = $_SERVER['REQUEST_URI'];
-// $dimp = str_contains($path, 'public') ? explode('public', $path)[1] : null;
-// echo($dimp);
-// echo Request::server('REQUEST_URI');
-// $script = dirname($_SERVER['SCRIPT_NAME']);
-// echo($script);
-// echo '<pre>';
-// $path = $_SERVER['REQUEST_urI'];
-// print_r($_SERVER);
+$route = new Route(new Request);
 
 
-// echo '</pre>';
-// $requestUri = Request::server('REQUEST_URI');
 
-// echo $requestUri;
-
-// if (strpos($requestUri, '?') !== false) {
-//     list($requestUri, $queryString) = explode('?', $requestUri);
-//  }
-
-//  $url = explode('public', $requestUri)[1];
-//  var_dump($url);
 
 
 
---
+++ src/Router/Route.php
@@ -0,0 +1,123 @@
+<?php 
+
+namespace Matariya\Router;
+use Matariya\Http\Request;
+
+class Route
+{    
+    /**
+     * routes container
+     *
+     * @var array
+     */
+    private $routes = [];
+
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    
+    /**
+     * adding routes to routes container
+     *
+     * @param  string $method
+     * @param  string $uri
+     * @param  mixed $action
+     * @return void
+     */
+    public function addRoute(string $requestMethod = 'GET', string $uri, callable|string|array $action)
+    {
+        $uri = trim($uri, '/');
+
+        $this->routes[] = [
+            'uri'            => $uri,
+            'method'         => $requestMethod,
+            'action'         => $action,
+        ];
+    }
+
+    
+    /**
+     * adding route through get method
+     *
+     * @param  mixed $url
+     * @param  mixed $action
+     * @return 
+     */
+    public function get($url, $action)
+    {
+         $this->addRoute('GET', $url, $action);
+    }
+    
+    /**
+     * adding route through post method
+     *
+     * @param  mixed $url
+     * @param  mixed $action
+     * @return 
+     */
+    public function post($url, $action)
+    {
+         $this->addRoute('post', $url, $action);
+    }
+
+    public function delete()
+    {
+        //  
+    }
+
+    public function put()
+    {
+        //
+    }
+    
+    /**
+     * handleRoute
+     *
+     * @return void
+     */
+    public function handleRoute()
+    {
+        $url = $this->request->getUrl();
+
+        foreach ($this->routes as $route) {
+            $matched = true;
+
+            $route['uri'] = preg_replace('/\/{(.*?)}/', '/(.*?)', $route['uri']);
+            $route['uri'] = '#^' .$route['uri'] .'$#';
+            
+            if (preg_match($route['uri'], $url, $matches)) {
+                array_shift($matches);
+
+                $params = array_values($matches);
+                foreach ($params as $param) {
+                    if (strpos($param, '/')) {
+                        $matched = false;
+                    }
+                }
+                
+                if ($route['method'] != Request::method()) {
+                    $matched = false;
+                }
+
+                if ($matched) {
+                    return $this->invoke($route, $params);
+                }
+            }
+
+        }
+    }
+
+
+    public function invoke($route, array $params = [])
+    {
+        $action = $route['action'];
+
+        if (is_callable($action)) {
+            return call_user_func($action);
+        }
+    }
+}
